2698fa6487c587cc1727c8e757f4ecdafa79aa74
TayLand <talandes@att.net>
2015-10-19 11:53:07 -0400

    Update Ex3_SummingTheGaps.asm

--- Ex3_SummingTheGaps.asm
+++ Ex3_SummingTheGaps.asm
@@ -6,14 +6,24 @@
 ExitProcess proto,dwExitCode:dword
 
 .data
-dwarray dword  0,2,5,9,10
-count EQU (LENGTHOF dwarray)
+array DWORD 0,2,5,9,10
+sum DWORD 0
 
 .code
-main proc
-	
-	;Your code goes here
-	
-	invoke ExitProcess,0
-main endp
-end main
+main PROC
+
+	mov edi, OFFSET array
+	mov ecx, LENGTHOF array
+
+	L1:
+		mov eax, [edi]
+		add edi, 4
+		mov ebx, [edi]
+		sub ebx, eax
+		add sum, ebx
+		loop L1
+
+		invoke ExitProcess, 0
+
+	main ENDP
+	END main
